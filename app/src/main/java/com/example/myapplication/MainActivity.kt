737567e9a6b27d94158709a8ec8cb5a0ba24cf1e
ahmedtmgouda <78615670+ahmedtmgouda@users.noreply.github.com>
2024-07-30 12:36:54 +0300

    My Requests Page Complete
    Adding the My Requests page activity and class. Still needs some minor tweaks.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.notifications.ui.theme.NotificationScreen
+import com.example.yourapp.MyRequestsActivity
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -18,11 +28,11 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
 //         Finallayout()
                 NotificationScreen()
+
             }
         }
     }
 }
-
 
 
 

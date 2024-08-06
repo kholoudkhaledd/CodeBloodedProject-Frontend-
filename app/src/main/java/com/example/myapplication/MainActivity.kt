fbf40bb290b4836b6f83edc9b44ff6e57e66acd2
kholoudkhaled <khaledkholoud3737@yahoo.com>
2024-08-06 13:13:41 +0300

    Firebase Connection and manager calander

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,30 +6,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.example.yourapp.ui.MyRequestsPage
-import com.example.yourapp.ui.Request
-import com.example.yourapp.ui.RequestStatus
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this) // Initialize Firebase
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                val sampleRequests = listOf(
-                    Request(id = 1, time = "8m ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.APPROVED),
-                    Request(id = 2, time = "8m ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.APPROVED),
-                    Request(id = 3, time = "10 days ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.DENIED),
-                    Request(id = 4, time = "15 days ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.DENIED),
-                    Request(id = 5, time = "20 days ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.DENIED)
-                )
-
-                ManagerRequest(requests = sampleRequests)
-
+//
+//            FirebaseConnection()
             }
         }
     }
 
-}
+

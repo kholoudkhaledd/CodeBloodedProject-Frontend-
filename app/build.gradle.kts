9a99318fbe1e5c306a9c8713013eb7d7d9faeffc
kholoud khaled <khaledkholoud3737@yahoo.com>
2024-08-06 21:48:54 +0300

    Connected android studio to firestore database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -71,6 +72,8 @@ dependencies {
     implementation (libs.compose)
 
     implementation(libs.androidx.activity)
+    implementation(libs.firebase.common.ktx)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,33 +2,57 @@ package com.example.myapplication
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.yourapp.ui.MyRequestsPage
 import com.example.yourapp.ui.Request
 import com.example.yourapp.ui.RequestStatus
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.database
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             MyApplicationTheme {
-                val sampleRequests = listOf(
-                    Request(id = 1, time = "8m ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.APPROVED),
-                    Request(id = 2, time = "8m ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.APPROVED),
-                    Request(id = 3, time = "10 days ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.DENIED),
-                    Request(id = 4, time = "15 days ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.DENIED),
-                    Request(id = 5, time = "20 days ago", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci", status = RequestStatus.DENIED)
-                )
-
-                ManagerRequest(requests = sampleRequests)
-
+                // Call your Firebase connection test function here
+                FirebaseConnection()
             }
+        }
+    }
+}
+@Composable
+fun FirebaseConnection() {
+    // Get a reference to the database
+    val database = Firebase.database
+    val myRef = database.getReference("message")
+
+    // Test reading from the database
+    myRef.get().addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            val data = task.result?.value
+            Log.d("FirebaseConnection", "Data: $data")
+        } else {
+            Log.e("FirebaseConnection", "Error getting data", task.exception)
+        }
+    }
+
+    // Test writing to the database
+    myRef.setValue("Hello, Firebase!").addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            Log.d("FirebaseConnection", "Data written successfully")
+        } else {
+            Log.e("FirebaseConnection", "Error writing data", task.exception)
         }
     }
 }

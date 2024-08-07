f8a88947cd3bab342af0703dd7a6fe4400dbfe8a
kholoudkhaled <khaledkholoud3737@yahoo.com>
2024-08-07 10:45:45 +0300

    Added ManagerCalanderview and organized manager pages

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,19 +2,13 @@ package com.example.myapplication
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.runtime.Composable
+import com.example.myapplication.manager.Viewteamscalander
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.example.yourapp.ui.MyRequestsPage
-import com.example.yourapp.ui.Request
-import com.example.yourapp.ui.RequestStatus
-import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
-import com.google.firebase.database.database
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -26,33 +20,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // Call your Firebase connection test function here
-                NavigationScreen()
+//                NavigationScreen()
+                Viewteamscalander()
             }
         }
     }
 }
-@Composable
-fun FirebaseConnection() {
-    // Get a reference to the database
-    val database = Firebase.database
-    val myRef = database.getReference("message")
-
-    // Test reading from the database
-    myRef.get().addOnCompleteListener { task ->
-        if (task.isSuccessful) {
-            val data = task.result?.value
-            Log.d("FirebaseConnection", "Data: $data")
-        } else {
-            Log.e("FirebaseConnection", "Error getting data", task.exception)
-        }
-    }
-
-    // Test writing to the database
-    myRef.setValue("Hello, Firebase!").addOnCompleteListener { task ->
-        if (task.isSuccessful) {
-            Log.d("FirebaseConnection", "Data written successfully")
-        } else {
-            Log.e("FirebaseConnection", "Error writing data", task.exception)
-        }
-    }
-}
+//@Composable
+//fun FirebaseConnection() {
+//    // Get a reference to the database
+//    val database = Firebase.database
+//    val myRef = database.getReference("message")
+//
+//    // Test reading from the database
+//    myRef.get().addOnCompleteListener { task ->
+//        if (task.isSuccessful) {
+//            val data = task.result?.value
+//            Log.d("FirebaseConnection", "Data: $data")
+//        } else {
+//            Log.e("FirebaseConnection", "Error getting data", task.exception)
+//        }
+//    }
+//
+//    // Test writing to the database
+//    myRef.setValue("Hello, Firebase!").addOnCompleteListener { task ->
+//        if (task.isSuccessful) {
+//            Log.d("FirebaseConnection", "Data written successfully")
+//        } else {
+//            Log.e("FirebaseConnection", "Error writing data", task.exception)
+//        }
+//    }
+//}
---
+++ app/src/main/java/com/example/myapplication/manager/ManagerCalanderview.kt
@@ -0,0 +1,202 @@
+package com.example.myapplication.manager
+
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.calander.CalendarViewScreen
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun Viewteamscalander() {
+    // List of names
+    val namesList = listOf("Alice", "Bob", "Charlie", "Diana")
+
+    // State to manage the selected name and expanded state of dropdown
+    var selectedName by remember { mutableStateOf<String?>(null) }
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .padding(vertical = 15.dp)
+            .clip(RoundedCornerShape(20.dp))
+//            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(vertical = 15.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(Color.White)
+        ) {
+
+            // First row with text at the start
+            Text(
+                text = "Select a name",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(vertical = 10.dp, horizontal = 10.dp)
+            )
+
+            // Dropdown for selecting names
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp, horizontal = 10.dp)
+                    .wrapContentSize(Alignment.TopStart)
+                    .clip(RoundedCornerShape(10.dp))
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.LightGray)
+                        .clip(RoundedCornerShape(8.dp))
+                        .padding(vertical = 5.dp, horizontal = 10.dp)
+                        .clickable { expanded = true }
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = selectedName ?: "Select team member ",
+                            color = Color.Black,
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(end = 8.dp)
+                        )
+
+                        Icon(
+                            imageVector = Icons.Filled.KeyboardArrowDown,
+                            contentDescription = "Dropdown Icon",
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    namesList.forEach { name ->
+                        DropdownMenuItem(
+                            text = { Text(name, color = Color.Black) },
+                            onClick = {
+                                selectedName = name
+                                expanded = false
+                            },
+                            modifier = Modifier.background(Color.White)
+                        )
+                    }
+                }
+            }
+
+            // Box displaying the selected name
+            if (selectedName != null) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp))
+
+                ) {
+                    Caladarmanager()
+                }
+            }
+        }
+    }
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+fun DropdownPreview() {
+    CalendarViewScreen()
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun Caladarmanager() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFECECEC)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        item {
+            // Display if home or office Section
+            Box(
+                modifier = Modifier
+                    .background(Color(0xFFECECEC))
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp , horizontal = 10.dp)
+            ) {
+                Text("Click on day to change between office&home")
+            }
+        }
+
+        item {
+            // Calendar View Screen
+            Box(
+                modifier = Modifier
+                    .background(Color(0xFFECECEC))
+                    .fillMaxWidth()
+                    .padding(0.dp)
+                    .clip(RoundedCornerShape(25.dp))
+                    .height(450.dp)
+            ) {
+                CalendarViewScreen()
+            }
+        }
+        item {    Button(
+            onClick = { /* Handle submit action */ },
+            colors = ButtonDefaults.buttonColors(Color.LightGray),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp, horizontal = 10.dp)
+        ) {
+            Text("Save")
+        } }
+
+    }
+}
+
